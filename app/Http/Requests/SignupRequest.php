cde74e90403dc122f03aa29fd6cab489c46ea1ec
yousafshah1214 <kodemania5@gmail.com>
2016-04-10 11:19:36 +0500

    Signup User

---
+++ app/Http/Requests/SignupRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class SignupRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username'      =>      'required|string|min:6|unique:users,username',
+            'password'      =>      'required|string|min:6',
+            'email'         =>      'required|email'
+        ];
+    }
+}
